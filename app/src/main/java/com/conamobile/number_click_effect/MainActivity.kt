f9da99d248d2bb474b658b759a1655793a7ffae5
ConaMobile <conamobiledev@gmail.com>
2022-01-31 18:00:09 +0500

    Number Click Effect app

---
+++ app/src/main/java/com/conamobile/number_click_effect/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.conamobile.number_click_effect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
